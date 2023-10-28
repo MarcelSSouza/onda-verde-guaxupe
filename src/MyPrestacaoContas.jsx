e02d369f71b43c518c263a43eb4e44eb0c891607
MarcelSSouza <marcel.santos@leaf.space>
2023-10-28 12:49:49 +0100

    adding stuff

---
+++ src/MyPrestacaoContas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyPrestacaoContas() {
+  return (
+    <div>MyPrestacaoContas</div>
+  )
+}
--- src/MyQuemSomos.jsx
+++ src/MyQuemSomos.jsx
@@ -16,17 +16,8 @@ export default function MyQuemSomos() {
         </Col>
         <Col md={6}>
           <p>
-            A ONG Onda Ver Guaxupé é uma organização dedicada ao bem-estar e à proteção de gatos e cachorros na região de Guaxupé. Nosso compromisso é oferecer abrigo, cuidados e encontrar lares amorosos para animais desabrigados ou em situações de risco.
-          </p>
-          <p>
-            Nossa equipe é formada por apaixonados por animais que dedicam seu tempo e esforço para fazer a diferença na vida desses seres incríveis. Juntos, trabalhamos arduamente para garantir que cada gato e cachorro receba a atenção, o cuidado e o amor que merecem.
-          </p>
-          <p>
-            Além de resgatar e reabilitar animais, também promovemos a conscientização sobre a importância da esterilização, vacinação e cuidados adequados com animais de estimação. Acreditamos que a educação é fundamental para criar uma comunidade que trata todos os seres vivos com compaixão.
-          </p>
-          <p>
-            Junte-se a nós nessa missão de fazer do mundo um lugar melhor para os animais. Sua ajuda, apoio e amor são essenciais para o sucesso de nosso trabalho. Juntos, podemos criar um futuro mais brilhante para gatos e cachorros em Guaxupé.
-          </p>
+          A Associação Onda Verde foi fundada em 30 de outubro de 2021, e registrada em março de 2022, tendo como propósito trazer ao município de Guaxupé o  desenvolvimento nas áreas ambiental, social e animal.     A associação tem por finalidade a saúde única por meio da integração social através da defesa de bens e direitos sociais, coletivos e difusos relacionados à saúde humana e animal, ao meio ambiente, ao patrimônio cultural, aos direitos humanos e dos povos, por meio da elaboração de projetos teóricos e ações práticas com foco nas particularidades geográficas e necessidades básicas de cada grupo populacional da região de abrangência da Onda Verde.          </p>
+
         </Col>
       </Row>
 <br />
@@ -34,22 +25,17 @@ export default function MyQuemSomos() {
 
       <Row>
         
-      <h2 className="text-center ">Nossas ações</h2>
+      <h2 className="text-center ">Nossos Projetos</h2>
       <hr />
 
       <Col md={6}>
           <p>
-            A ONG Onda Ver Guaxupé é uma organização dedicada ao bem-estar e à proteção de gatos e cachorros na região de Guaxupé. Nosso compromisso é oferecer abrigo, cuidados e encontrar lares amorosos para animais desabrigados ou em situações de risco.
-          </p>
-          <p>
-            Nossa equipe é formada por apaixonados por animais que dedicam seu tempo e esforço para fazer a diferença na vida desses seres incríveis. Juntos, trabalhamos arduamente para garantir que cada gato e cachorro receba a atenção, o cuidado e o amor que merecem.
-          </p>
-          <p>
-            Além de resgatar e reabilitar animais, também promovemos a conscientização sobre a importância da esterilização, vacinação e cuidados adequados com animais de estimação. Acreditamos que a educação é fundamental para criar uma comunidade que trata todos os seres vivos com compaixão.
-          </p>
-          <p>
-            Junte-se a nós nessa missão de fazer do mundo um lugar melhor para os animais. Sua ajuda, apoio e amor são essenciais para o sucesso de nosso trabalho. Juntos, podemos criar um futuro mais brilhante para gatos e cachorros em Guaxupé.
-          </p>
+          Foram efetuados em 2022 e 2023 projetos de castração em parceria com o Fórum de Guaxupé.   Os recursos financeiros são oriundos de sentenças que se destinam para as Associações de Guaxupé, que buscam sua aprovação através de projetos.  Por 02 (dois) anos consecutivos a ONG Onda Verde teve seus projetos aprovados.   Com esses recursos, foram castrados 23 (vinte e três) cães em 2022 e 100 (cem) cães em 2023.    Importante destacar que os cães castrados são de famílias carentes.
+
+<p>
+Em andamento existem, ainda, um Programa de reflorestamento elaborado por algumas voluntárias,  bem como o Reiki Solidário e o Reiki para todos, que realiza visitas onde são oferecidos três tratamentos aos idosos da entidade Vila Vicentina Frederico Ozanam: diálogo fraterno, acupuntura e Reiki aos que desejem receber o cuidado fraterno, funcionários e assistidos          </p>
+</p>
+
         </Col>
         <Col md={6}>
           <img
