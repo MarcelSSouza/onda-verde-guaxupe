94d7707b23f21e1c74bbade227e0adc7ee69819c
Fabio-Matos1303 <fabiosmo123@gmail.com>
2023-11-02 03:26:43 -0300

    Feat: Adicionando MyNoticias

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from './Navbar';
+import React from 'react';
 
 
 
@@ -10,16 +11,18 @@ import MyFooter from './MyFooter';
 import MyFormularioAdocao from './MyFormularioAdocao';
 import MyFormularioContato from './MyFormularioContato';
 import MyAjuda from './MyAjuda';
+import MyNoticias from './MyNoticias';
 function App() {
   return (
     <div className="App">
-          <MyNavbar></MyNavbar>
-        <BrowserRouter>
+      <MyNavbar></MyNavbar>
+      <BrowserRouter>
         <Routes>
           <Route index element={<Main />} />
-          <Route path="/contato" element={<MyFormularioContato />} /> 
-          <Route path="/ajuda" element={<MyAjuda />} /> 
-          <Route path="/adocao" element={<MyFormularioAdocao />} /> 
+          <Route path="/contato" element={<MyFormularioContato />} />
+          <Route path="/ajuda" element={<MyAjuda />} />
+          <Route path="/adocao" element={<MyFormularioAdocao />} />
+          <Route path="/noticias" element={<MyNoticias />} />
         </Routes>
       </BrowserRouter>
       <MyFooter></MyFooter>
