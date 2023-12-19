dccde5ddd809dd0aa321ceae89e233e5b760af06
MarcelSSouza <marcel.santos@leaf.space>
2023-12-19 17:47:10 +0000

    Adding stuff

--- src/MyFacebook.jsx
+++ src/MyFacebook.jsx
@@ -6,7 +6,6 @@ export default function MyFacebook() {
   <div className="bg-success text-white">
     <h1>Nosso Facebook</h1>
   </div>
-  <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fondaverdegxp&tabs=timeline&width=500&height=499&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="500" height="499"   frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-  </>
+  <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fondaverdegxp&tabs=timeline&width=350&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="350" height="500" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>  </>
   );
 }
